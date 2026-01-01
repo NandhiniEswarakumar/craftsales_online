c164d821e8c88ba953bc89e755b7e77e4050e58b
Nandhini-E <nandhinie.23it@kongu.edu>
2026-01-01 22:08:04 +0530

    Resolve merge conflict in jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
@@ -7,14 +6,4 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
-=======
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testPathIgnorePatterns: ['/node_modules/'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
->>>>>>> 8e1af95a (Clean repo: remove build/node_modules and update dependencies)
 };
